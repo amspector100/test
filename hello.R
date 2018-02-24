ffe85ec10913129d452b95f45385fef121f5c2a1
Asher Spector <amspector100@gmail.com>
2018-02-24 14:17:59 -0500

    This will create a merge conflict

--- hello.R
+++ hello.R
@@ -3,12 +3,14 @@
 
 loss <- function(x, y){
   
-  # Calculate error
-  error <- abs(x-y)
+  # Calculate squared error
+  error <- (x-y)**2
   
   # Print and return it
   print(sprintf('Here is the squared error: %s', as.character(error)))
   return(error)
 }
+
+
 
 
